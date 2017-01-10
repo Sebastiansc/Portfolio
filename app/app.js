d0d1f51717a5e41c4d01dc9ca05d0adc51f8f7fd
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2017-01-09 19:18:45 -0800

    Add scrolling navigation and nav bar color changes

--- app/app.js
+++ app/app.js
@@ -10,5 +10,22 @@ document.addEventListener("DOMContentLoaded", () => {
     $('nav').toggleClass('hidden-xs');
     $('.fa-bars').toggle();
   });
+
+  $('#port-link').click( () => {
+    $('html,body').animate({
+        scrollTop: $(".portfolio").offset().top + 20},
+        'slow');
+  });
+
+  $(document).scroll( () => {
+    if ($('nav').offset().top > $('.portfolio').offset().top
+        || $('.fa-bars').offset().top > $('.portfolio').offset().top) {
+      $('nav').addClass('light-nav');
+      $('.fa-bars').addClass('light-bars');
+    } else {
+      $('nav').removeClass('light-nav');
+      $('.fa-bars').removeClass('light-bars');
+    }
+  });
 });
 // Nav Bar end
