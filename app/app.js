8c8a0ffbc36116ab5ffa3ca98269b2c35b2ac925
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2017-01-10 15:41:36 -0800

    Add animations

--- app/app.js
+++ app/app.js
@@ -1,5 +1,13 @@
 // Nav Bar
 document.addEventListener("DOMContentLoaded", () => {
+  $('.animation-wrapper').hide();
+  $('.about-wrapper').hide();
+  $('.contact-wrapper').hide();
+
+  $('.home').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+    $('.home h2').addClass('animated tada');
+  });
+
   $('.fa-bars').click( e => {
     $('.fa-bars').toggle('fast', () => {
       $('nav').toggleClass('hidden-xs');
@@ -40,6 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let portfolioTop = $('.portfolio').offset().top;
     let barsTop = $('.fa-bars').offset().top;
     let aboutTop = $('.about').offset().top;
+    let contactTop = $('.contact').offset().top;
     if (navTop > aboutTop || barsTop > aboutTop){
       $('nav').removeClass('light-nav');
       $('.fa-bars').removeClass('light-bars');
@@ -50,6 +59,23 @@ document.addEventListener("DOMContentLoaded", () => {
       $('nav').removeClass('light-nav');
       $('.fa-bars').removeClass('light-bars');
     }
+
+    if ($(document).scrollTop() > portfolioTop - 250) {
+      $('.animation-wrapper').show();
+      $('.animation-wrapper').addClass('animated fadeIn');
+    }
+
+    if ($(document).scrollTop() > aboutTop - 350) {
+      $('.about-wrapper').show();
+      $('.about-wrapper').addClass('animated fadeIn');
+    }
+
+    if ($(document).scrollTop() > contactTop - 350) {
+      $('.contact-wrapper').show();
+      $('.contact-wrapper').addClass('animated fadeIn');
+      $('.contact ul li').addClass('animated swing');
+    }
   });
+
 });
 // Nav Bar end
